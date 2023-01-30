49fdb6bebafc6c323945b4d2a85877efc3ea0d6b
haritselmanan <123790673+haritselmanan17@users.noreply.github.com>
2023-01-30 08:21:27 +0700

    Create hitung_umur.py

---
+++ hitung_umur.py
@@ -0,0 +1,19 @@
+import datetime as dt
+
+print("Silahkan masukan tanggal,\nbulan dan tahun lahir anda \n")
+tanggal = int (input("tanggal \t: "))
+bulan = int (input("bulan \t\t: "))
+tahun = int (input("tahun\t\t: "))
+
+tanggal_lahir = dt.date(tahun,bulan,tanggal)
+print(f"Tanggal Lahir Anda Adalah: {tanggal_lahir}")
+
+
+hari_ini = dt.date.today()
+print(f"Hari Ini Tanggal: {hari_ini}")
+umur_hari = hari_ini - tanggal_lahir
+umur_tahun = umur_hari.days //365
+umur_bulan_sisa = (umur_hari.days % 365) //30
+
+print(f"Hari nya adalah : {tanggal_lahir:%A}")
+print(f"Umur anda adalah : {umur_tahun} tahun, {umur_bulan_sisa} bulan")
